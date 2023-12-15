5fcb9a8ee2f6917e9d8a2f5e182ff17ced960cf0
hido <bestdohee@gmail.com>
2023-12-15 15:47:59 +0900

    fix: fundings로 수정

--- server/routes/v1/index.ts
+++ server/routes/v1/index.ts
@@ -8,7 +8,7 @@ const v1Router = Router();
 //User
 v1Router.use("/auth", authRouter);
 
-v1Router.use("/funding", fundingRouter);
+v1Router.use("/fundings", fundingRouter);
 
 v1Router.use("/user", userRouter);
 
