f457e2f2e7027172f0753aca7b8bd2356d54d340
hido <bestdohee@gmail.com>
2023-12-18 20:45:32 +0900

    fix: userService 타입 지정

--- server/services/userService.ts
+++ server/services/userService.ts
@@ -1,7 +1,15 @@
-import { userModel } from "../models"; // 사용자 모델 임포트
+import { userModel } from "../models";
+
+interface UpdateUserData {
+  password?: string;
+  phoneNumber?: string;
+  postCode?: number;
+  address?: string;
+  addressDetail?: string;
+}
 
 const userService = {
-  async getUserProfile(userId: any) {
+  async getUserProfile(userId: string) {
     try {
       const user = await userModel.findById(userId);
 
@@ -14,7 +22,7 @@ const userService = {
       throw error;
     }
   },
-  async deleteUser(userId: any) {
+  async deleteUser(userId: string) {
     try {
       const user = await userModel.findById(userId);
 
@@ -26,6 +34,41 @@ const userService = {
 
       await user.save();
       return { message: "다음에 또 만나요!" };
+    } catch (error) {
+      throw error;
+    }
+  },
+  async updateUserProfile(userId: string, updateData: UpdateUserData) {
+    try {
+      const user = await userModel.findById(userId);
+
+      if (!user) {
+        throw new Error("사용자를 찾을 수 없습니다.");
+      }
+
+      if (updateData.password) {
+        user.password = updateData.password;
+      }
+
+      if (updateData.phoneNumber) {
+        user.phoneNumber = updateData.phoneNumber;
+      }
+
+      if (updateData.postCode) {
+        user.postCode = updateData.postCode;
+      }
+
+      if (updateData.address) {
+        user.address = updateData.address;
+      }
+
+      if (updateData.addressDetail) {
+        user.addressDetail = updateData.addressDetail;
+      }
+
+      await user.save();
+
+      return { success: true, message: "사용자 정보가 업데이트되었습니다." };
     } catch (error) {
       throw error;
     }
