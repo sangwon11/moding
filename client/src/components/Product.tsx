e45d140a673521c87f5221161294943bed39dd6b
최윤혁 <johan43311@gmail.com>
2023-12-20 21:23:24 +0900

    Fix

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -1,25 +1,51 @@
 import React from "react"
-
+import { useNavigate } from "react-router-dom"
+import tw from "tailwind-styled-components"
 interface ProductProps {
-    id: number
+    id: string
     url: string
     alt: string
     description: string // 상품 설명
     manufacturer: string // 제조사
+    isSlider?: boolean // 슬라이더에서 사용되는지 여부를 나타내는 prop
 }
 
-function Product({ id, url, alt, description, manufacturer }: ProductProps) {
+// Product 컴포넌트
+function Product({ id, url, alt, description, manufacturer, isSlider }: ProductProps) {
+    //console.log("Image URL:", url);
+    //console.log("Image URL:", "바보");
+    const containerClassName = isSlider ? "w-full" : "w-1/3"
+    const navigate = useNavigate()
     return (
-        <div key={id} className="w-1/3 px-2 mb-4 ">
-            <div className="w-160 h-64 overflow-hidden rounded-lg bg-gray-200">
-                <img src={url} alt={alt} className="object-cover object-center w-full h-full" />
-            </div>
-            <div className="text-sm mt-3">
-                <p className="text-white text-lg font-semibold mb-3">{description}</p>{" "}
-                <p className="text-gray-500 text-sm">{manufacturer}</p>{" "}
-            </div>
+        <div onClick={() => navigate("/funding", { state: id })} className={`${containerClassName} px-2 mb-4`} key={id}>
+            <ImageContainer>
+                <Image src={url} alt={alt} />
+            </ImageContainer>
+            <DescriptionContainer>
+                <Description>{description}</Description>
+                <Manufacturer>{manufacturer}</Manufacturer>
+            </DescriptionContainer>
         </div>
     )
 }
+// 스타일 컴포넌트
+const ImageContainer = tw.div`
+  w-full h-64 overflow-hidden rounded-lg bg-gray-200
+`
 
+const Image = tw.img`
+  object-cover object-center w-full h-full
+`
+
+const DescriptionContainer = tw.div`
+  text-sm mt-3
+`
+
+const Description = tw.p`
+  text-white text-lg font-semibold mb-3
+`
+
+const Manufacturer = tw.p`
+  text-gray-500 text-sm
+`
 export default Product
