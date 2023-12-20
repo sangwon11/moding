5c5004bd54d4557332797bfcadc7b982c504b739
최윤혁 <johan43311@gmail.com>
2023-12-20 21:21:06 +0900

    Fix

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -1,29 +1,25 @@
-import React from "react";
+import React from "react"
 
 interface ProductProps {
-  id: number;
-  url: string;
-  alt: string;
-  description: string; // 상품 설명
-  manufacturer: string; // 제조사
+    id: number
+    url: string
+    alt: string
+    description: string // 상품 설명
+    manufacturer: string // 제조사
 }
 
 function Product({ id, url, alt, description, manufacturer }: ProductProps) {
-  return (
-    <div key={id} className="w-1/3 px-2 mb-4 ">
-      <div className="w-160 h-64 overflow-hidden rounded-lg bg-gray-200">
-        <img
-          src={url}
-          alt={alt}
-          className="object-cover object-center w-full h-full"
-        />
-      </div>
-      <div className="text-sm mt-3">
-        <p className="text-white text-lg font-semibold mb-3">{description}</p>{" "}
-        <p className="text-gray-500 text-sm">{manufacturer}</p>{" "}
-      </div>
-    </div>
-  );
+    return (
+        <div key={id} className="w-1/3 px-2 mb-4 ">
+            <div className="w-160 h-64 overflow-hidden rounded-lg bg-gray-200">
+                <img src={url} alt={alt} className="object-cover object-center w-full h-full" />
+            </div>
+            <div className="text-sm mt-3">
+                <p className="text-white text-lg font-semibold mb-3">{description}</p>{" "}
+                <p className="text-gray-500 text-sm">{manufacturer}</p>{" "}
+            </div>
+        </div>
+    )
 }
 
-export default Product;
+export default Product
