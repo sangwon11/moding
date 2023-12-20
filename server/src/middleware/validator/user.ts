77f7a512a3ceca1d5269a434449524a5b9135ff1
hido <bestdohee@gmail.com>
2023-12-20 23:18:54 +0900

    fix: updateuservalidator 안 되던 오류 해결

--- server/src/middleware/validator/user.ts
+++ server/src/middleware/validator/user.ts
@@ -1,20 +1,17 @@
 const { body } = require("express-validator");
 
 const updateUserValidator = [
-  body("password")
-    .optional()
+  body("updateData.password")
     .isLength({ min: 8 })
     .withMessage("비밀번호는 8자리 이상이어야 합니다."),
-  body("phoneNumber")
-    .optional()
+  body("updateData.phoneNumber")
     .notEmpty()
     .withMessage("전화번호를 입력해주세요."),
-  body("postCode")
-    .optional()
+  body("updateData.postCode")
     .notEmpty()
     .withMessage("우편번호를 입력해주세요."),
-  body("address").optional().notEmpty().withMessage("주소를 입력해주세요."),
-  body("addressDetail").optional(),
+  body("updateData.address").notEmpty().withMessage("주소를 입력해주세요."),
+  body("updateData.addressDetail").optional(),
 ];
 
 export { updateUserValidator };
