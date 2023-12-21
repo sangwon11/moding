1afaeb85658a680c70bd0c8221ea1171ddd82edd
최윤혁 <johan43311@gmail.com>
2023-12-21 23:30:10 +0900

    Add: main화면 업데이트

--- client/src/components/Product.tsx
+++ client/src/components/Product.tsx
@@ -1,29 +1,30 @@
 import React from "react"
 import { useNavigate } from "react-router-dom"
 import tw from "tailwind-styled-components"
+import { formatPercentage } from "../utils/format.utils"
 interface ProductProps {
     id: string
     url: string
     alt: string
     description: string // 상품 설명
-    manufacturer: string // 제조사
     isSlider?: boolean // 슬라이더에서 사용되는지 여부를 나타내는 prop
+    currentAmount?: number // 현재 모금액
+    goalAmount?: number // 목표 모금액
 }
 
 // Product 컴포넌트
-function Product({ id, url, alt, description, manufacturer, isSlider }: ProductProps) {
-    //console.log("Image URL:", url);
-    //console.log("Image URL:", "바보");
+function Product({ id, url, alt, description, isSlider, currentAmount, goalAmount }: ProductProps) {
     const containerClassName = isSlider ? "w-full" : "w-1/3"
+    const percentage = currentAmount && goalAmount ? formatPercentage(currentAmount, goalAmount) : "0"
     const navigate = useNavigate()
     return (
-        <div onClick={() => navigate("/funding", { state: id })} className={`${containerClassName} px-2 mb-4`} key={id}>
+        <div className={`${containerClassName} px-2 mb-4`} key={id}>
             <ImageContainer>
-                <Image src={url} alt={alt} />
+                <Image src={url} alt={alt} onClick={() => navigate("/funding", { state: id })} />
             </ImageContainer>
             <DescriptionContainer>
                 <Description>{description}</Description>
-                <Manufacturer>{manufacturer}</Manufacturer>
+                <Manufacturer>{`${percentage}% 달성`}</Manufacturer>
             </DescriptionContainer>
         </div>
     )
--- client/src/pages/main/MainPage.styles.tsx
+++ client/src/pages/main/MainPage.styles.tsx
@@ -5,22 +5,22 @@ import tw from "tailwind-styled-components"
 // }
 // 메인 페이지 컨테이너
 export const MainPageContainer = tw.div`
-  main-page
+  main-page 
 `
 // 컨텐츠 영역
 export const ContentArea = tw.div`
-  relative mt-10 flex justify-between items-start
+  relative mt-10 flex justify-between items-start max-w-[1440px] mx-auto
 `
 // 섹션
 export const Section = tw.div`
-  w-3/4 p-4 space-y-4
+  w-3/4 p-4 space-y-4 max-w-[1440px] mx-auto
 `
 export const ImageListContainer = tw.div`
-  flex flex-wrap -mx-2
+  flex flex-wrap -mx-2 max-w-[1440px] mx-auto
 `
 // 탭 컨테이너
 export const TabButtonsContainer = tw.div`
-  flex space-x-2 mb-4
+  flex space-x-2 mb-4 max-w-[1440px] mx-auto
 `
 // 탭 버튼
 export const TabButton = tw.button`
@@ -31,11 +31,11 @@ export const TabButton = tw.button`
 //   ${({ isActive }) => (isActive ? "bg-[#333333]" : "")}
 // `;
 export const TabContentContainer = tw.div`
-  p-4 rounded bg-[#D9D9D9]/[.1] 
+  p-4 rounded bg-[#D9D9D9]/[.1]
 `
 // 스티키(우측) 섹션
 export const StickySection = tw.div`
-   sticky top-10 w-1/4 mr-4 p-4 space-y-3 rounded bg-[#D9D9D9]/[.1] mt-[72.5px] mb-4
+   sticky top-10 w-1/4 mr-4 p-4 space-y-3 rounded bg-[#D9D9D9]/[.1] mt-[72.5px] mb-4 max-w-[1440px] mx-auto
 `
 // 실시간 랭킹 타이틀
 export const RealTimeRankingTitle = tw.h2`
@@ -47,10 +47,10 @@ export const RealTimeRankingList = tw.ul`
 `
 // 전체 너비를 사용하는 슬라이더 컨테이너 스타일
 export const FullWidthSliderContainer = tw.div`
-w-4/4 p-4 space-y-4
+w-4/4 p-4 space-y-4 max-w-[1440px] mx-auto
 `
 // "얼리버드" 타이틀 스타일
 export const EarlyBirdTitle = tw.h2`
   // text-4xl font-bold my-4 text-white relative left-4
-  text-4xl font-bold text-center my-4 text-white
+  text-4xl font-bold text-center my-4 text-white max-w-[1440px] mx-auto
 `
