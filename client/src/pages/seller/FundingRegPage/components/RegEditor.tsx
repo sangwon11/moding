d48a06d4baeb0dcf6b76843eb9397545541ac400
weaall <weaall88@gmail.com>
2023-12-23 01:53:50 +0900

    Fix : seller page

--- client/src/pages/seller/FundingRegPage/components/RegEditor.tsx
+++ client/src/pages/seller/FundingRegPage/components/RegEditor.tsx
@@ -4,16 +4,59 @@ import Editor from "ckeditor5-custom-build/build/ckeditor"
 
 import "./ckediter/CKEditer.styles.css"
 import { editorConfig } from "./ckediter/CKEditer.config"
-import uploadPlugin from "./ckediter/CKEditer.function"
 import * as styled from "./../FundingRegPage.styles"
+import { axiosInstance } from "../../../../utils/axios.utils"
 
 function RegEditor() {
-    const [editorData, setEditorData] = useState("");
+    const [editorData, setEditorData] = useState("")
 
     const handleEditorChange = (event: any, editor: any) => {
-        const data = editor.getData();
-        setEditorData(data);
-    };
+        const data = editor.getData()
+        setEditorData(data)
+    }
+
+    // const [formData, setFormData] = useState({
+    //     seller: "",
+    //     title: "",
+    //     category: "",
+    //     mainImageUrl: "",
+    //     imageUrls: [],
+    //     goalAmout: "",
+    //     currentAmount: 0,
+    //     starteDate: new Date(),
+    //     endDate: new Date(),
+    //     preorder: false,
+    //     preorderDate: new Date(),
+    //     deliveryPrice: "",
+    //     deliveryDate: new Date(),
+    //     preorderBenefits: [],
+    //     options: [],
+    // })
+
+    // const addFunding = async () => {
+    //     try {
+    //         const config = await frontEndAuthMiddleware({
+    //             method: "post",
+    //             url: "/seller",
+    //             data: formData,
+    //         })
+
+    //         const response = await axiosInstance.request(config)
+    //         if (response.status === 201) {
+    //             window.alert("펀딩이 등록되었습니다.")
+    //             navigate("/")
+    //         } else {
+    //         }
+    //     } catch (error) {
+    //         if (axios.isAxiosError(error) && error.response) {
+    //             if (error.response.status === 409) {
+    //                 window.alert("펀딩등록에 실패했습니다.")
+    //             } else {
+    //                 window.alert("펀딩등록에 실패했습니다.")
+    //             }
+    //         }
+    //     }
+    // }
 
     return (
         <styled.CKEitorWrap>
@@ -22,7 +65,29 @@ function RegEditor() {
                 data=""
                 config={editorConfig}
                 onReady={(editor: any) => {
-                    uploadPlugin(editor)
+                    editor.plugins.get("FileRepository").createUploadAdapter = (loader: any) => {
+                        return {
+                            upload: async () => {
+                                const formData = new FormData()
+                                formData.append("image", loader.file)
+
+                                try {
+                                    const response = await axiosInstance.post("/upload", formData, {
+                                        headers: {
+                                            "Content-Type": "multipart/form-data",
+                                        },
+                                    })
+
+                                    return {
+                                        default: response.data.url,
+                                    }
+                                } catch (error) {
+                                    console.error("이미지 업로드 에러", error)
+                                    throw error
+                                }
+                            },
+                        }
+                    }
                 }}
                 onChange={handleEditorChange}
             />
--- client/src/pages/seller/SellerPage.tsx
+++ client/src/pages/seller/SellerPage.tsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom"
-import FundingRegPage from "./FundingRegPage/FundingRegPage"
 import SellerNav from "./SellerNav/SellerNav"
 import RegRouter from "./FundingRegPage/RegisterRouter"
 
