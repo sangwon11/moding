90241c9f46c3a473d4505126e0ccd5f3262b9308
hido <bestdohee@gmail.com>
2023-12-22 16:45:20 +0900

    fix: 배송 정보 업데이트 api 삭제

--- server/src/services/sellerService.ts
+++ server/src/services/sellerService.ts
@@ -87,32 +87,6 @@ const sellerService = {
       throw new CustomError("펀딩 정보를 수정할 수 없습니다.", 403);
     }
   },
-  async deliveryUpdate(
-    fundingId: string,
-    deliveryNumber: string,
-    deliveryType: string
-  ) {
-    try {
-      const funding = await fundingModel.findById(fundingId);
-
-      if (!funding) {
-        return { success: false, message: "펀딩을 찾을 수 없습니다." };
-      }
-
-      funding.deliveryNumber = deliveryNumber;
-      funding.deliveryType = deliveryType;
-
-      await funding.save();
-
-      return {
-        success: true,
-        message: "배송 정보가 성공적으로 업데이트되었습니다.",
-      };
-    } catch (error) {
-      console.log(error);
-      throw new CustomError("배송 정보를 업데이트할 수 없습니다.", 500);
-    }
-  },
 };
 
 export default sellerService;
