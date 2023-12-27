4b550328bac2d7597f175d77c54b0672e8fb7652
weaall <weaall88@gmail.com>
2023-12-27 12:58:09 +0900

    Fix : payment

--- client/src/pages/payment/PaymentPage.tsx
+++ client/src/pages/payment/PaymentPage.tsx
@@ -1,10 +1,11 @@
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 import * as styled from "./PaymentPage.styles"
 import Payment from "../../components/portone/Pay"
 import { useState, useEffect } from "react"
 import { optionsProps } from "../../interface/schema.interface"
 import { frontEndAuthMiddleware } from "../../utils/jwtUtils"
 import { axios, axiosInstance } from "../../utils/axios.utils"
+import { formatPrice } from "../../utils/format.utils"
 
 interface OrderMethodProps {
     label: string
@@ -12,6 +13,7 @@ interface OrderMethodProps {
 }
 
 function PaymentPage() {
+    const navigate = useNavigate()
     const state = useLocation().state
     const funding = state.funding
     const optionSelect = state.optionSelect
@@ -25,33 +27,31 @@ function PaymentPage() {
         phoneNumber: "",
         postCode: "",
         address: "",
-        adressDetail: "",
+        addressDetail: "",
     })
 
     const [order, setOrder] = useState({
-        userId: "",
-        orderedBy: "",
-        postCode: "",
-        address: "",
-        addressDetail: "",
-        phoneNumber: "",
+        userId: "asd",
+        orderedBy: "asd",
+        postCode: "asd",
+        address: "asd",
+        addressDetail: "asd",
+        phoneNumber: "asd",
         fundingId: funding._id,
         orderList: optionSelect,
-        donation : supPrice,
-        nameOpen : true,
-        priceOpen : true,
-        orderStatus : "주문완료"
+        donation: supPrice,
+        nameOpen: true,
+        priceOpen: true,
+        orderStatus: "주문완료",
     })
 
     const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
+        const { name, value } = e.target
 
-        
         const sanitizedValue =
-          name === "phoneNumber" ? value.replace(/[^0-9]/g, "").replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3') : value;
-          setOrder(prevOrder => ({
-            ...prevOrder, [name]: sanitizedValue }));
-      };
+            name === "phoneNumber" ? value.replace(/[^0-9]/g, "").replace(/(\d{3})(\d{4})(\d{4})/, "$1-$2-$3") : value
+        setOrder({...order,[name]: sanitizedValue,})
+    }
 
     const fetchUser = async () => {
         try {
@@ -73,21 +73,19 @@ function PaymentPage() {
         }
     }
 
-    const formatPrice = (num: number) => {
-        return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-    }
-
     const [totalPrice, setTotalPrice] = useState(0)
 
     useEffect(() => {
-        fetchUser()
+        if (userInfo._id.length === 0) {
+            fetchUser()
+        }
         const sumTotalPrice =
             selectedOptions.reduce((acc: number, item: optionsProps) => acc + Number(item.price), 0) +
             Number(supPrice) +
             Number(funding.deliveryPrice)
 
         setTotalPrice(sumTotalPrice)
-    },[])
+    }, [])
 
     const [orderIndex, setOrderIndex] = useState<number>(0)
     const [orderPg, setOrderPg] = useState<string>("tosspay")
@@ -116,7 +114,29 @@ function PaymentPage() {
         buyer_addr: "신사동 661-16", // 구매자 주소
         buyer_postcode: "06018", // 구매자 우편번호
     }
-    
+
+    const insertData = async () => {
+        try {
+          const response = await axiosInstance.post("/orders", order);
+          if (response.status === 201) {
+            window.alert("성공적으로 결제되었습니다.");
+          } else {
+          }
+        } catch (error) {
+          if (axios.isAxiosError(error) && error.response) {
+            if (error.response.status === 409) {
+              window.alert("결제에 실패하였습니다.");
+            } else {
+            }
+          }
+        }
+      };
+
+    const nextClick = () => {
+        console.log(order)
+        insertData()
+    }
+
     return (
         <styled.Container>
             <styled.ContentsWrap>
@@ -143,29 +163,22 @@ function PaymentPage() {
                 <styled.MeWrap>
                     <styled.MeLabel>주문자정보</styled.MeLabel>
                     <styled.InputWrap>
-                        <styled.Input value={userInfo.email} onChange={onChangeInput} disabled></styled.Input>
+                        <styled.Input value={userInfo.email} onChange={onChangeInput} disabled />
                         <styled.Input
-                            defaultValue={userInfo.username}
+                            value={order.orderedBy}
                             onChange={onChangeInput}
                             name="orderedBy"
-                        ></styled.Input>
+                        />
                         <styled.Input
-                            defaultValue={userInfo.phoneNumber}
+                            value={order.phoneNumber}
                             onChange={onChangeInput}
                             name="phoneNumber"
-                        ></styled.Input>
+                            maxLength={13}
+                        />
+                        <styled.Input value={order.postCode} onChange={onChangeInput} name="postCode"></styled.Input>
+                        <styled.Input value={order.address} onChange={onChangeInput} name="address"></styled.Input>
                         <styled.Input
-                            defaultValue={userInfo.postCode}
-                            onChange={onChangeInput}
-                            name="postCode"
-                        ></styled.Input>
-                        <styled.Input
-                            defaultValue={userInfo.address}
-                            onChange={onChangeInput}
-                            name="address"
-                        ></styled.Input>
-                        <styled.Input
-                            defaultValue={userInfo.adressDetail}
+                            value={order.addressDetail}
                             onChange={onChangeInput}
                             name="addressDetail"
                         ></styled.Input>
@@ -186,13 +199,7 @@ function PaymentPage() {
                 </styled.OrderWrap>
                 <styled.NextWrap>
                     <Payment data={paymentData}></Payment>
-                    <styled.NextBtn
-                        onClick={() => {
-                            console.log(order)
-                        }}
-                    >
-                        결제하기
-                    </styled.NextBtn>
+                    <styled.NextBtn onClick={nextClick}>결제하기</styled.NextBtn>
                 </styled.NextWrap>
             </styled.ContentsWrap>
         </styled.Container>
