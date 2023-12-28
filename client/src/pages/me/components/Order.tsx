f6a3b3aed37ec68b9c4804ba8875e1be94bf0ce6
weaall <weaall88@gmail.com>
2023-12-28 18:22:30 +0900

    Fix : footer

--- client/src/pages/me/components/Order.tsx
+++ client/src/pages/me/components/Order.tsx
@@ -4,10 +4,13 @@ import axios from "axios"
 import { axiosInstance } from "../../../utils/axios.utils"
 import { frontEndAuthMiddleware } from "../../../utils/jwtUtils"
 import * as styled from "../MyPage.styles"
+import OrderList from "./OrderList"
+import Loading from "../../../components/loading/Loading"
 
 function Order() {
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     const userInfo = useLocation().state.userInfo
+    const [loading, setLoading] = useState(true)
 
     const [orderList, setOrderList] = useState([
         {
@@ -42,59 +45,42 @@ function Order() {
 
             const response = await axiosInstance.request(config)
             setOrderList(response.data.data)
-            console.log(response.data.data)
         } catch (error) {
             if (axios.isAxiosError(error) && error.response) {
                 if (error.response.status === 409) {
                     window.alert("올바른 접근이 아닙니다.")
+                    navigate("/404")
                 } else {
                     window.alert("올바른 접근이 아닙니다.")
                 }
             }
-        }
-    }
-
-    const fetchProduct = async () => {
-        try {
-            const config = await frontEndAuthMiddleware({
-                method: "get",
-                url: "/orders/" + userInfo._id,
-            })
-
-            const response = await axiosInstance.request(config)
-            setOrderList(response.data.data)
-            console.log(response.data.data)
-        } catch (error) {
-            if (axios.isAxiosError(error) && error.response) {
-                if (error.response.status === 409) {
-                    window.alert("올바른 접근이 아닙니다.")
-                } else {
-                    window.alert("올바른 접근이 아닙니다.")
-                }
-            }
+        }   finally {
+            setLoading(false)
         }
     }
 
     useEffect(() => {
-        if(!userInfo){
-            window.alert("올바른 접근이 아닙니다.")
-            navigate("/404")
-        }
         fetchOrder()
     }, [])
+
+    if (loading) {
+        return <Loading />
+    }
 
     return (
         <styled.OrdersWrap>
             <styled.PageLabel>구매목록</styled.PageLabel>
-            {orderList.map((item, index) => (
-                <styled.OrderWrap key={index}>
-                    <styled.OrderNumber>
-                        주문날짜:{item.updatedAt.slice(0, 10)} 
-                        {item.updatedAt.slice(11, 16)}
-                    </styled.OrderNumber>
-                    <styled.OrderNumber>주문번호:{item.orderNumber}</styled.OrderNumber>
-                </styled.OrderWrap>
-            ))}
+            <styled.TagWrap>
+                <styled.Tag200>주문일자</styled.Tag200>
+                <styled.Tag250>상품정보</styled.Tag250>
+                <styled.Tag200>주문번호</styled.Tag200>
+                <styled.Tag100>배송시작</styled.Tag100>
+            </styled.TagWrap>
+            {orderList
+                .sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime())
+                .map((item, index) => {
+                    return <OrderList {...item} />
+                })}
         </styled.OrdersWrap>
     )
 }
